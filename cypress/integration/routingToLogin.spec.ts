33fde307451f0e0cee4a27c1293b70a0253ef2e5
Generaladvisor Lam <113605832+Generaladvisor-Lam@users.noreply.github.com>
2023-09-11 14:05:59 +0200

    generated test examples

--- cypress/integration/routingToLogin.spec.ts
+++ cypress/integration/routingToLogin.spec.ts
@@ -3,5 +3,6 @@ import 'cypress';
 describe('Test Suite', () => {
     it('Should visit loginpage', () => {
         cy.visit('http://localhost:3000/login');
+        cy.title().should('eq', 'React App')
     })
 })
